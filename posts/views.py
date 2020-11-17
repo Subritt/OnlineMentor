721dba81a6fbe86d24d2a8eebb07d929a6480d3e
Subritt <subrittburlakoti@gmail.com>
2020-11-17 21:55:52 +0545

    created a dummy database in views.py and passed the context to home.html template

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Jane Doe',
+        'title': 'Query 1',
+        'content': 'FIrst query',
+        'date_posted': 'November 17, 2020'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Query 2',
+        'content': 'Second query',
+        'date_posted': 'November 18, 2020'
+    }
+]
+
 def home(request):
-    return render(request, 'posts/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'posts/home.html', context)
