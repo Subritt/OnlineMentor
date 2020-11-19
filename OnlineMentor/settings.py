f386b22f56f31a771c58d9cd5ab0ffbb0306a3a3
Subritt <subrittburlakoti@gmail.com>
2020-11-19 22:10:55 +0545

    initialized app in settings, created a register view

--- OnlineMentor/settings.py
+++ OnlineMentor/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.app.UsersConfig',
     'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
