169fca5afdea3d69866e5883f6023365538b675b
Subritt <subrittburlakoti@gmail.com>
2020-11-23 19:38:17 +0545

    added a post/new rout and created query_form.html template for creating new post

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import (
     PostListView,
-    PostDetailView
+    PostDetailView,
+    PostCreateView
 )
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home-page'),
-    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail')
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/new', PostDetailView.as_view(), name='post-create')
 ]
