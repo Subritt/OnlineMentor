c20e38568887c08971cf18d21fd75174c76f2263
Subritt <subrittburlakoti@gmail.com>
2020-11-23 22:24:10 +0545

    imported UserPassesTestMixin and checked condition to grant user the permission to edit authorized post

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 from django.views.generic import (
     ListView,
     DetailView,
@@ -25,10 +28,14 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Query
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
