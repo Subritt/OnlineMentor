8eded7a268382c499822ffbbd6d80217007e1e26
Subritt <subrittburlakoti@gmail.com>
2020-11-25 00:18:06 +0545

    imported get_object_or_404 class, defined UserPostListView class, and set route

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     UserPassesTestMixin
@@ -18,6 +19,16 @@ class PostListView(ListView):
     context_object_name = 'queries'
     ordering = ['-date_posted']
     paginate_by = 9
+
+class UserPostListView(ListView):
+    model = Query
+    template_name = 'posts/home.html'
+    context_object_name = 'queries'
+    paginate_by = 9
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Query.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Query
