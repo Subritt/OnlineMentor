19753541dd99ab0ad61b72c015b04e9075497a67
Subritt <subrittburlakoti@gmail.com>
2020-11-28 18:59:27 +0545

    created a Reply model to store replies for every Query model

--- posts/models.py
+++ posts/models.py
@@ -14,3 +14,12 @@ class Query(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+class Reply(models.Model):
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    post = models.ForeignKey(Query, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.post.title
