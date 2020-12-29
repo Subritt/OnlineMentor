bc50557c5196cf8b7a7350d42904d5234345e28d
Rajesh <rajeshkarki500@gmail.com>
2020-12-29 11:23:41 +0545

    same code just updated

--- OnlineMentor/settings.py
+++ OnlineMentor/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,5 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
 
 STATIC_URL = '/static/'
