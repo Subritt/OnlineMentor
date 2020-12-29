13691c4d474172a526add9818e4bef1220310c27
Rajesh <rajeshkarki500@gmail.com>
2020-12-29 11:22:25 +0545

    views of templates created

--- home/views.py
+++ home/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+
+    return render(request, 'home.html')
+
+
+def about(request):
+
+    return render(request, "about.html")
+
+
+def tutor(request):
+
+    return render(request, "tutor.html")
+
+
+def contactus(request):
+
+    return render(request, "contactus.html")
+
+
+def signin(request):
+
+    return render(request, "signin.html")
+
+
+def signup(request):
+
+    return render(request, "signup.html")
+
+
+def blog(request):
+
+    return render(request, "blog.html")
