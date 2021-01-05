66024470d6e030b1493c6940d30c1650a90dc22d
Subritt <subrittburlakoti@gmail.com>
2021-01-05 21:31:58 +0545

    installed CK Editor, added RichTextEditor in models and reflected it to template

--- posts/models.py
+++ posts/models.py
@@ -2,21 +2,29 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.text import slugify
+from ckeditor.fields import RichTextField
 
 class Query(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = RichTextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    header = models.SlugField(max_length=1000)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+    
+    def save(self, *args, **kwargs):
+        self.header = slugify(self.title)
+        return super().save(*args, **kwargs)
+    
 
 class Reply(models.Model):
-    content = models.TextField()
+    content = RichTextField()
     date_posted = models.DateTimeField(default=timezone.now)
     post = models.ForeignKey(Query, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
